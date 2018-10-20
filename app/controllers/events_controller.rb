482d7a561498ef59ce38477b47c22b34d8c910c0
Edwin Tandiono <tandiono98@gmail.com>
2018-10-20 12:52:50 +0700

    Add event create and show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,27 @@
 class EventsController < ApplicationController
+	skip_before_action :verify_authenticity_token
 
 	def index
-
 		@events = Event.all
-
-		respond_to do |format|
-			format.html
-			format.json { render json:@events }
-		end
-		
+		render json:@events
 	end
+
+	def show
+		@event = Event.find(params[:id])
+		render json:@event
+	end
+
+	def create
+		puts(params)
+		@event = Event.new(event_params)
+		if @event.save
+			render html: 'saved.'
+		end
+	end
+
+	private
+		def event_params
+	  	params.require(:event).permit(:name, :description)
+		end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'events/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/home'
+
+  match 'events/' => 'events#index', :via => [:get]
+  match 'events/:id' => 'events#show', :via => [:get]
+  match 'events/create' => 'events#create', :via => :post
 
   root 'application#hello'
 
